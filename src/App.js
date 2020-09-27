fa19fddeabf463bc5e81f8d4f1b5d9c9d4fbb422
Srinibas Biswal <srinibasbiswal12@gmail.com>
2020-09-28 00:11:40 +0530

    Add Base files

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from "react";
 import "./App.css";
+import HomePage from "./pages/HomePage";
 
 function App() {
-    return <div className="App"></div>;
+    return (
+        <div className="App">
+            <HomePage />
+        </div>
+    );
 }
 
 export default App;
---
+++ src/pages/HomePage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import HeaderContainer from "../containers/HeaderContainer";
+
+function HomePage() {
+    return (
+        <div>
+            <HeaderContainer />
+        </div>
+    );
+}
+
+export default HomePage;
