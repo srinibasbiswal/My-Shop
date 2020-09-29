701a0e9b53d5367be472e26fead3b81bf8872ee5
Srinibas Biswal <srinibasbiswal12@gmail.com>
2020-09-30 00:18:22 +0530

    Modify Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,63 +6,43 @@ function Footer() {
         <div>
             <hr className={`uk-divider-icon`} />
             <div
-                className={`uk-child-width-expand@s uk-text-center`}
+                class="uk-grid-divider uk-child-width-expand@s uk-margin-small-bottom"
                 uk-grid={"true"}
             >
                 <div>
-                    <div>
-                        <ul className={`uk-nav-primary uk-text-center`}>
-                            <li className={`uk-navbar-item`}>
-                                <a
-                                    className={`uk-logouk-padding-remove`}
-                                    href="#"
-                                >
-                                    Logo
-                                </a>
-                            </li>
-                            <li className={`uk-navbar-item`}>
-                                <p>
-                                    &#169; 2020 All rights reserved. Company
-                                    Name
-                                </p>
-                            </li>
-                        </ul>
-                    </div>
+                    <ul className={`uk-nav-primary uk-text-center`}>
+                        <li className={`uk-navbar-item`}>
+                            <a className={`uk-logouk-padding-remove`} href="#">
+                                Logo
+                            </a>
+                        </li>
+                        <li className={`uk-navbar-item`}>
+                            <p>&#169; 2020 All rights reserved. Company Name</p>
+                        </li>
+                    </ul>
                 </div>
                 <div>
-                    <div>
-                        <ul className={`uk-text-center`}>
-                            <li
-                                className={`uk-navbar-item ${styles.FooterLink}`}
-                            >
-                                Link 1
-                            </li>
-                            <li
-                                className={`uk-navbar-item ${styles.FooterLink}`}
-                            >
-                                Link 2
-                            </li>
-                            <li
-                                className={`uk-navbar-item ${styles.FooterLink}`}
-                            >
-                                Link 3
-                            </li>
-                            <li
-                                className={`uk-navbar-item ${styles.FooterLink}`}
-                            >
-                                Link 4
-                            </li>
-                        </ul>
-                    </div>
+                    <ul className={`uk-text-center`}>
+                        <li className={`uk-navbar-item ${styles.FooterLink}`}>
+                            Link 1
+                        </li>
+                        <li className={`uk-navbar-item ${styles.FooterLink}`}>
+                            Link 2
+                        </li>
+                        <li className={`uk-navbar-item ${styles.FooterLink}`}>
+                            Link 3
+                        </li>
+                        <li className={`uk-navbar-item ${styles.FooterLink}`}>
+                            Link 4
+                        </li>
+                    </ul>
                 </div>
                 <div>
-                    <div>
-                        <p className={`uk-text-lead`}>Contact Us</p>
-                        <p>
-                            Address 1 <br /> Address 2 <br /> Address 3 <br />{" "}
-                            Phone : 000 000 0000
-                        </p>
-                    </div>
+                    <p className={`uk-text-lead uk-text-center`}>Contact Us</p>
+                    <p className={`uk-text-center`}>
+                        Address 1 <br /> Address 2 <br /> Address 3 <br /> Phone
+                        : 000 000 0000
+                    </p>
                 </div>
             </div>
         </div>
