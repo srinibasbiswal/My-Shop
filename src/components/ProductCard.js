cda19e1893d6cae56fae1b4cf2794d4b189ea738
Srinibas Biswal <srinibasbiswal12@gmail.com>
2020-10-05 23:30:23 +0530

    Change button name in product card

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -15,11 +15,13 @@ function ProductCard() {
                     <br />
                     <strong>&#x20B9; 90</strong>
                 </p>
-                <button
-                    className={`uk-button uk-button-primary ${styles.textColorWhite} ${styles.backgroundPrimary} uk-border-rounded`}
-                >
-                    Buy
-                </button>
+                <div>
+                    <button
+                        className={`uk-button uk-button-primary ${styles.textColorWhite} ${styles.backgroundPrimary} uk-border-rounded uk-width-1-1 uk-padding-remove`}
+                    >
+                        Add to cart
+                    </button>
+                </div>
             </div>
         </div>
     );
