7d2cbab7855d7597f20aa358a595a74ccd6521f0
Srinibas Biswal <srinibasbiswal12@gmail.com>
2020-10-10 17:23:25 +0530

    Add search functionality for item search

--- src/containers/SearchResult.js
+++ src/containers/SearchResult.js
@@ -2,9 +2,12 @@ import React, { useEffect, useState } from "react";
 import CategoriesCards from "../components/CategoriesCards";
 import ResultProducts from "../components/ResultProducts";
 import { searchTypes } from "../data/enums/searchType";
+import { products } from "../data/prodcuts";
 
 function SearchResult(props) {
 	const [showCategoryCards, showshowCategoryCards] = useState(true);
+	const [productList, setProductList] = useState([]);
+
 	useEffect(() => {
 		function setCategoryShowValue() {
 			if (
@@ -14,8 +17,31 @@ function SearchResult(props) {
 				showshowCategoryCards(false);
 			}
 		}
+
+		function getProducts() {
+			if (props.searchType !== undefined && props.search !== undefined) {
+				var filteredProducts = [];
+				switch (props.searchType) {
+					case searchTypes.ITEM:
+						filteredProducts = products.filter(function (item) {
+							return item.name
+								.toLowerCase()
+								.includes(props.search.toLowerCase());
+						});
+						setProductList(filteredProducts);
+						break;
+					case searchTypes.CATEGORY:
+						break;
+
+					default:
+						break;
+				}
+			}
+		}
+
 		setCategoryShowValue();
-	}, [props.searchType]);
+		getProducts();
+	}, [props.searchType, props.search]);
 
 	if (props !== undefined && props.search === "") {
 		return (
@@ -26,6 +52,7 @@ function SearchResult(props) {
 			</div>
 		);
 	} else if (props !== undefined && props.search !== undefined) {
+		console.log(productList);
 		return (
 			<React.Fragment>
 				{showCategoryCards ? (
