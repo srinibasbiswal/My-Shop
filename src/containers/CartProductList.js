8e4638cb9081bfefa76c183aef7f4b1039844d28
Srinibas Biswal <srinibasbiswal12@gmail.com>
2020-10-12 21:29:47 +0530

    Add cart page data loading

--- src/containers/CartProductList.js
+++ src/containers/CartProductList.js
@@ -1,26 +1,51 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import CartProduct from "../components/CartProduct";
+import { useDispatch, useSelector } from "react-redux";
+import { products } from "../data/prodcuts";
 
 function CartProductList() {
-    return (
-        <div>
-            <CartProduct />
-            <hr />
-            <CartProduct />
-            <hr />
-            <CartProduct />
-            <hr />
-            <CartProduct />
-            <hr />
-            <CartProduct />
-            <hr />
-            <CartProduct />
-            <hr />
-            <CartProduct />
-            <hr />
-            <CartProduct />
-        </div>
-    );
+	const dispatch = useDispatch();
+	const cartState = useSelector((state) => state.cart);
+
+	const [productDetailsMap, setProductDetailsMap] = useState({});
+
+	useEffect(() => {
+		function getProductDetailsMap() {
+			if (
+				cartState !== undefined &&
+				cartState.itemCodes !== undefined &&
+				cartState.itemCodes !== []
+			) {
+				var itemDetailMap = {};
+				products.filter(function (item) {
+					if (cartState.itemCodes.includes(item.id)) {
+						itemDetailMap[item.id] = item;
+					}
+				});
+				setProductDetailsMap(itemDetailMap);
+			}
+		}
+		getProductDetailsMap();
+	}, [cartState.itemCodes]);
+
+	return (
+		<div>
+			<hr />
+			<CartProduct />
+			<hr />
+			<CartProduct />
+			<hr />
+			<CartProduct />
+			<hr />
+			<CartProduct />
+			<hr />
+			<CartProduct />
+			<hr />
+			<CartProduct />
+			<hr />
+			<CartProduct />
+		</div>
+	);
 }
 
 export default CartProductList;
