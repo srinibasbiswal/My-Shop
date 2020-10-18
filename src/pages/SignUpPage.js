5974aa001e384144342af81bc3425d54f6814079
Srinibas Biswal <srinibasbiswal12@gmail.com>
2020-10-18 20:57:34 +0530

    Add Phase 1 sign in page

--- src/pages/SignUpPage.js
+++ src/pages/SignUpPage.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Footer from "../components/Footer";
 import SignupForm from "../components/Forms/SignupForm";
+import SigninForm from "../components/Forms/SigninForm";
 import Header from "../components/Header";
 
 function SignUpPage() {
@@ -8,6 +9,7 @@ function SignUpPage() {
 		<React.Fragment>
 			<Header />
 			<SignupForm />
+			<SigninForm />
 			<Footer />
 		</React.Fragment>
 	);
