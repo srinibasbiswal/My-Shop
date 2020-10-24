b5f7ca3df0b9fb543b496ed478de581eb195c735
Srinibas Biswal <srinibasbiswal12@gmail.com>
2020-10-25 01:35:05 +0530

    Add user Document

---
+++ src/documents/UserDocument.js
@@ -0,0 +1,14 @@
+class UserDocument {
+	constructor() {
+		this.userName = "";
+		this.firstName = "";
+		this.lastName = "";
+		this.email = "";
+		this.phoneNumber = "";
+		this.authType = "";
+		this.isVerified = false;
+		this.userDefined = {};
+	}
+}
+
+export default UserDocument;
