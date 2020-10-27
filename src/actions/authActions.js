1438a95014e11c72ee4edcc6bd8c6327e6a43b69
Srinibas Biswal <srinibasbiswal12@gmail.com>
2020-10-28 01:50:06 +0530

    Add authstate from user

--- src/actions/authActions.js
+++ src/actions/authActions.js
@@ -10,6 +10,7 @@ import {
 } from "../firebase/authHandler";
 import { resetAmount } from "./amountActions";
 import { resetCart } from "./cartActions";
+import AuthStateDocument from "../documents/AuthStateDocument";
 
 export const logIn = (authType, email, password, rememberMe) => {
 	console.log(authType, email, password, rememberMe);
@@ -69,5 +70,29 @@ export const setAuthState = (authState) => {
 	return {
 		type: authResponses.SET_AUTH_STATE,
 		authState: authState,
+	};
+};
+
+export const setAuthStateFromUser = (user) => {
+	console.log(user);
+	var authStateDoc = new AuthStateDocument();
+	if (user) {
+		if (user.uid !== undefined && user.uid !== "") {
+			authStateDoc.userId = user.uid;
+			authStateDoc.isLoggedIn = true;
+			if (user.email !== undefined && user.email !== "") {
+				authStateDoc.userName = user.email;
+				if (user.emailVerified !== undefined) {
+					authStateDoc.isVerified = user.emailVerified;
+				}
+			} else {
+				authStateDoc.userName = user.phoneNumber;
+				authStateDoc.isVerified = true;
+			}
+		}
+	}
+	console.log(authStateDoc);
+	return (dispatch) => {
+		dispatch(setAuthState(authStateDoc));
 	};
 };
