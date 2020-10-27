90a95968d55985ee9594dbb9e74cd782ef91e12f
Srinibas Biswal <srinibasbiswal12@gmail.com>
2020-10-28 01:12:43 +0530

    Add header style

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,6 +5,7 @@ import { searchTypes } from "../data/enums/searchType";
 import styles from "../stylesheets/style.module.css";
 import { useDispatch, useSelector } from "react-redux";
 import { logOut } from "../actions/authActions";
+import { FiShoppingCart, FiUser, FiMenu } from "react-icons/fi";
 
 function Header(props) {
 	const dispatch = useDispatch();
@@ -28,135 +29,132 @@ function Header(props) {
 	};
 
 	return (
-		<div uk-sticky="sel-target: .uk-navbar-container; cls-active: uk-navbar-sticky">
-			<nav
-				className={`uk-navbar-container uk-navbar ${styles.backgroundPrimary}`}
-			>
-				<div className={`uk-navbar-left`}>
-					<ul className={`uk-navbar-nav`}>
-						<li className={`uk-navbar-item`}>
-							<a
-								uk-toggle="target: #menuOverlay"
-								data-uk-icon="icon: menu"
-								className={`${styles.textColorWhite} uk-navbar-toggle uk-navbar-toggle-icon uk-hidden@m uk-padding-remove`}
-							></a>
-						</li>
-						<li className={`uk-navbar-item`}>
-							<Link to="/">
+		<React.Fragment>
+			<div uk-sticky="sel-target: .uk-navbar-container; cls-active: uk-navbar-sticky">
+				<nav
+					className={`uk-navbar-container uk-navbar ${styles.backgroundPrimary}`}
+				>
+					<div className={`uk-navbar-left`}>
+						<ul className={`uk-navbar-nav`}>
+							<li className={`uk-navbar-item`}>
+								<Link to="/">
+									<span
+										className={`uk-logo ${styles.textColorWhite} uk-padding-remove ${styles.logo}`}
+									>
+										Logo
+									</span>
+								</Link>
+							</li>
+							<li className={`uk-navbar-item`}>
+								<form onSubmit={handleSubmit}>
+									<input
+										className={`uk-input uk-form-width-large uk-border-rounded uk-visible@m`}
+										type="text"
+										placeholder="Search item1, item2 and more ... "
+										value={searchValue}
+										onChange={setSearch}
+									/>
+								</form>
+							</li>
+							<li className={`uk-navbar-item`}></li>
+						</ul>
+					</div>
+
+					<div className={`uk-navbar-right`}>
+						<ul className={`uk-navbar-nav`}>
+							{console.log(authentication)}
+							{(() => {
+								if (authentication.isLoggedIn) {
+									return (
+										<li className={`uk-navbar-item`}>
+											<p>Hi {authentication.userName}</p>
+											<button
+												className={`uk-button uk-button-default`}
+												onClick={(e) =>
+													logOutAccount(e)
+												}
+											>
+												Logout
+											</button>
+										</li>
+									);
+								}
+							})()}
+
+							<li className={`uk-navbar-item`}>
+								<Link to="/cart">
+									<FiShoppingCart
+										className={`${styles.textColorWhite} ${styles.iconSize}`}
+									/>
+								</Link>
+							</li>
+
+							<li className={`uk-navbar-item`}>
+								<Link to="/signup">
+									<FiUser
+										className={`${styles.textColorWhite} ${styles.iconSize}`}
+									/>
+								</Link>
+							</li>
+							<li className={`uk-navbar-item`}>
+								<FiMenu
+									uk-toggle="target: #menuOverlay"
+									className={`${styles.textColorWhite} ${styles.iconSize}`}
+								/>
+							</li>
+						</ul>
+					</div>
+				</nav>
+				<div
+					id="menuOverlay"
+					data-uk-offcanvas="mode: push; flip: true"
+					className={`uk-offcanvas uk-offcanvas-content`}
+				>
+					<div
+						className={`uk-offcanvas-bar ${styles.backgroundPrimary}`}
+					>
+						<ul
+							className={`uk-nav uk-nav-primary uk-nav-center uk-margin-auto-vertical`}
+						>
+							<li className={`uk-navbar-item`}>
 								<a
 									className={`uk-logo ${styles.textColorWhite} uk-padding-remove`}
 									href="#"
 								>
 									Logo
 								</a>
-							</Link>
-						</li>
-					</ul>
-				</div>
-				<div className={`uk-navbar-center`}>
-					<ul className={`uk-navbar-nav`}>
-						<li className={`uk-navbar-item`}>
-							<form onSubmit={handleSubmit}>
-								<input
-									className={`uk-input uk-form-width-large uk-border-rounded uk-visible@m`}
-									type="text"
-									placeholder="Search item1, item2 and more ... "
-									value={searchValue}
-									onChange={setSearch}
-								/>
-							</form>
-						</li>
-					</ul>
-				</div>
-
-				<div className={`uk-navbar-right`}>
-					<ul className={`uk-navbar-nav`}>
-						<li className={`uk-navbar-item`}>
-							<form onSubmit={handleSubmit}>
-								<input
-									className={`uk-input uk-form-width-medium uk-border-rounded uk-hidden@m uk-padding-remove-right`}
-									type="text"
-									placeholder="Search item1, item2 and more ... "
-									value={searchValue}
-									onChange={setSearch}
-								/>
-							</form>
-						</li>
-						{console.log(authentication)}
-						{(() => {
-							if (authentication.isLoggedIn) {
-								return (
-									<li className={`uk-navbar-item`}>
-										<p>Hi {authentication.userName}</p>
-										<button
-											className={`uk-button uk-button-default`}
-											onClick={(e) => logOutAccount(e)}
-										>
-											Logout
-										</button>
-									</li>
-								);
-							}
-						})()}
-
-						<li>
-							<Link to="/cart">
-								<a
-									href="#"
-									data-uk-icon="icon: cart"
-									className={`${styles.textColorWhite}`}
-								></a>
-							</Link>
-						</li>
-
-						<li className={`uk-visible@m`}>
-							<Link to="/signup">
-								<a
-									href="#"
-									data-uk-icon="icon: info"
-									className={`${styles.textColorWhite}`}
-								></a>
-							</Link>
-						</li>
-					</ul>
-				</div>
-			</nav>
-			<div
-				id="menuOverlay"
-				data-uk-offcanvas="mode: push"
-				className={`uk-offcanvas uk-offcanvas-content`}
-			>
-				<div className={`uk-offcanvas-bar ${styles.backgroundPrimary}`}>
-					<ul
-						className={`uk-nav uk-nav-primary uk-nav-center uk-margin-auto-vertical`}
-					>
-						<li className={`uk-navbar-item`}>
-							<a
-								className={`uk-logo ${styles.textColorWhite} uk-padding-remove`}
-								href="#"
-							>
-								Logo
-							</a>
-						</li>
-						<li className={`uk-active`}>
-							<button
-								className={`uk-offcanvas-close uk-close`}
-								data-uk-icon="icon: close"
-							></button>
-						</li>
-						<li className={`uk-parent`}>
-							<a href="#">Menu 1</a>
-						</li>
-						<li className={`uk-parent`}>
-							<a href="#">Menu 2</a>
-						</li>
-						<li className={`uk-parent`}>
-							<a href="#">Menu 3</a>
-						</li>
-					</ul>
+							</li>
+							<li className={`uk-active`}>
+								<button
+									className={`uk-offcanvas-close uk-close`}
+									data-uk-icon="icon: close"
+								></button>
+							</li>
+							<li className={`uk-parent`}>
+								<a href="#">Menu 1</a>
+							</li>
+							<li className={`uk-parent`}>
+								<a href="#">Menu 2</a>
+							</li>
+							<li className={`uk-parent`}>
+								<a href="#">Menu 3</a>
+							</li>
+						</ul>
+					</div>
 				</div>
 			</div>
-		</div>
+			<div className={`uk-hidden@m ${styles.searchBarSmall}`}>
+				<form onSubmit={handleSubmit}>
+					<input
+						className={`uk-input uk-border-rounded `}
+						type="text"
+						placeholder="Search item1, item2 and more ... "
+						value={searchValue}
+						onChange={setSearch}
+					/>
+				</form>
+			</div>
+		</React.Fragment>
 	);
 }
 
