f430e9643165302e646d172f3354cdb0f829b6bd
Srinibas Biswal <srinibasbiswal12@gmail.com>
2020-10-30 00:39:32 +0530

    add address actions for set and get

--- src/actions/addressActions.js
+++ src/actions/addressActions.js
@@ -1,8 +1,24 @@
 import { actionTypes } from "../data/enums/actionTypes";
+import { createNewAddress, getAddresses } from "../firebase/addressHandler";
+import store from "../store";
+
+export const setAddress = (addressDoc) => {
+	return {
+		type: actionTypes.SET_ADDRESS,
+		address: addressDoc,
+	};
+};
+
+export const setAddressState = (addresses) => {
+	return {
+		type: actionTypes.SET_MULTIPLE_ADDRESSES,
+		addressList: addresses,
+	};
+};
 
 export const addAddress = (addressDoc) => {
-	return {
-		type: actionTypes.ADD_ADDRESS,
-		address: addressDoc,
+	return function (dispatch) {
+		createNewAddress(store.getState().authentication, addressDoc);
+		getAddresses(dispatch, store.getState().authentication.userId);
 	};
 };
