96f0d5833a8bea802cc4fd030a21e6a7524e624f
Srinibas Biswal <srinibasbiswal12@gmail.com>
2020-10-31 02:45:55 +0530

    Add switcher for address slider

--- src/components/AddressSlider.js
+++ src/components/AddressSlider.js
@@ -2,7 +2,8 @@ import React from "react";
 import { useDispatch } from "react-redux";
 import { setAddress } from "../actions/addressActions";
 import styles from "../stylesheets/style.module.css";
-import { FiCheckSquare } from "react-icons/fi";
+import { FiCheckSquare, FiPlusSquare } from "react-icons/fi";
+import NewAddress from "./Forms/NewAddress";
 
 function AddressSlider(props) {
 	const dispatch = useDispatch();
@@ -13,43 +14,80 @@ function AddressSlider(props) {
 
 	return (
 		<div className={`uk-card uk-card-default uk-card-body`}>
-			<h3 className={`uk-card-title`}>Select An Address</h3>
 			<div
-				class="uk-grid-column-small uk-grid-row-large uk-child-width-1-3@s uk-text-center"
-				uk-grid={`true`}
-				uk-height-match={`true`}
+				className={`switcherParent uk-margin-small-bottom`}
+				uk-switcher="animation: uk-animation-fade; toggle: > *"
 			>
-				{props.addressList.map((address, index) => {
-					if (props.selectedAddress.id === address.id) {
-						return (
-							<div>
-								<div
-									className={`uk-card uk-card-default uk-padding-small uk-border-rounded ${styles.buttonGradient}`}
-								>
-									<div
-										class="uk-alert-success uk-border-rounded"
-										uk-alert={`true`}
-									>
-										<FiCheckSquare />
-										<span> Selected</span>
-									</div>
-									{props.selectedAddress.toString()}
-								</div>
-							</div>
-						);
-					} else {
-						return (
-							<div onClick={() => onSelectAddress(address)}>
-								<div
-									className={`uk-card uk-card-default uk-padding-small uk-border-rounded ${styles.buttonGradient} ${styles.secondaryButton}`}
-								>
-									{address.toString()}
-								</div>
-							</div>
-						);
-					}
-				})}
+				<button
+					class="uk-button uk-button-default uk-margin-medium-right uk-border-rounded  uk-width-1-1@s uk-width-1-3@m uk-margin-small-bottom"
+					type="button"
+				>
+					Select An Address{" "}
+					<FiCheckSquare className={styles.iconSize} />
+				</button>
+				<button
+					class="uk-button uk-button-default uk-border-rounded  uk-width-1-1@s uk-width-1-3@m uk-margin-small-bottom"
+					type="button"
+				>
+					Add New Address <FiPlusSquare className={styles.iconSize} />
+				</button>
 			</div>
+			<hr />
+
+			<ul class="uk-switcher uk-margin">
+				<li>
+					<div
+						class="uk-grid-column-small uk-grid-row-large uk-child-width-1-3@s uk-text-center"
+						uk-grid={`true`}
+						uk-height-match={`true`}
+					>
+						{props.addressList.map((address, index) => {
+							if (props.selectedAddress.id === address.id) {
+								return (
+									<div>
+										<div
+											className={`uk-card uk-card-default uk-padding-small uk-border-rounded ${styles.buttonGradient}`}
+										>
+											<div
+												class="uk-alert-success uk-border-rounded"
+												uk-alert={`true`}
+											>
+												<FiCheckSquare />
+												<span> Selected</span>
+											</div>
+											{props.selectedAddress.toString()}
+										</div>
+									</div>
+								);
+							} else {
+								return (
+									<div
+										onClick={() => onSelectAddress(address)}
+									>
+										<div
+											className={`uk-card uk-card-default uk-padding-small uk-border-rounded ${styles.buttonGradient} ${styles.secondaryButton}`}
+										>
+											{address.toString()}
+										</div>
+									</div>
+								);
+							}
+						})}
+						<div>
+							<div
+								className={`uk-card uk-card-default uk-card-body uk-border-rounded ${styles.buttonGradient} ${styles.secondaryButton}`}
+								uk-switcher-item="1"
+							>
+								Add a new address{" "}
+								<FiPlusSquare className={styles.iconSize} />
+							</div>
+						</div>
+					</div>
+				</li>
+				<li>
+					<NewAddress />
+				</li>
+			</ul>
 		</div>
 	);
 }
