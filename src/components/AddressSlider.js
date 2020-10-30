b2e903eb77d61d3544e1a9deb875e0d9c47a4a24
Srinibas Biswal <srinibasbiswal12@gmail.com>
2020-10-31 01:22:07 +0530

    Redesign address slider

--- src/components/AddressSlider.js
+++ src/components/AddressSlider.js
@@ -1,64 +1,51 @@
 import React from "react";
 import { useDispatch } from "react-redux";
 import { setAddress } from "../actions/addressActions";
+import styles from "../stylesheets/style.module.css";
+import { FiCheckSquare } from "react-icons/fi";
 
 function AddressSlider(props) {
 	const dispatch = useDispatch();
-	const onSelectAddress = (address) => {
-		dispatch(setAddress(address));
+	const onSelectAddress = (e) => {
+		console.log(e.target.value);
+		dispatch(setAddress(e.target.value));
 	};
 
 	return (
 		<div className={`uk-card uk-card-default uk-card-body`}>
 			<h3 className={`uk-card-title`}>Select An Address</h3>
 			<div
-				className={`uk-position-relative uk-visible-toggle uk-light uk-card-body`}
-				tabIndex="-1"
-				uk-slider={`true`}
+				class="uk-grid-column-small uk-grid-row-large uk-child-width-1-3@s uk-text-center"
+				uk-grid={`true`}
+				uk-height-match={`true`}
 			>
-				<ul
-					className={`uk-slider-items uk-child-width-1-1 uk-child-width-1-3@m uk-grid`}
-				>
-					<li>
+				<div>
+					<div
+						className={`uk-card uk-card-default uk-padding-small uk-border-rounded ${styles.buttonGradient}`}
+					>
 						<div
-							className={`uk-card uk-card-default uk-card-small uk-card-body uk-box-shadow-large uk-card-hover`}
+							class="uk-alert-success uk-border-rounded"
+							uk-alert={`true`}
 						>
-							<input
-								class="uk-checkbox"
-								type="checkbox"
-								checked
-							/>{" "}
-							<span
-								className={`uk-text-success uk-text-uppercase`}
-							>
-								Selected
-							</span>
-							<p>{props.selectedAddress.toString()}</p>
+							<FiCheckSquare />
+							<span> Selected</span>
 						</div>
-					</li>
-					{props.addressList.map((address, index) => {
+						{props.selectedAddress.toString()}
+					</div>
+				</div>
+				{props.addressList.map((address, index) => {
+					if (props.selectedAddress.id !== address.id) {
 						return (
-							<li>
-								{(() => {
-									if (
-										props.selectedAddress.id !== address.id
-									) {
-										return (
-											<div
-												className={`uk-card uk-card-default uk-card-small uk-card-body uk-card-hover`}
-												onClick={(address) =>
-													onSelectAddress(address)
-												}
-											>
-												<p>{address.toString()}</p>
-											</div>
-										);
-									}
-								})()}
-							</li>
+							<div>
+								<div
+									className={`uk-card uk-card-default uk-padding-small uk-border-rounded ${styles.buttonGradient} ${styles.secondaryButton}`}
+								>
+									{address.toString()}
+								</div>
+							</div>
 						);
-					})}
-				</ul>
+					}
+				})}
 			</div>
 		</div>
 	);
